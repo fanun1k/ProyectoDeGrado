71e2f2fcf3b2d97f86189a504cd5b56d9c10ae16
fanun1k <pablors0598@gmail.com>
2022-04-23 18:48:54 -0400

    se agrego las paginas base para desarrollar las vistas

--- app/Views/header_footer/sidebar.php
+++ app/Views/header_footer/sidebar.php
@@ -252,9 +252,9 @@
 						</ul>
 					</li>
 			
-			</ul><!-- /.nav-list -->
+				</ul><!-- /.nav-list -->
 
-			<div class="sidebar-toggle sidebar-collapse" id="sidebar-collapse">
-				<i id="sidebar-toggle-icon" class="ace-icon fa fa-angle-double-left ace-save-state" data-icon1="ace-icon fa fa-angle-double-left" data-icon2="ace-icon fa fa-angle-double-right"></i>
+				<div class="sidebar-toggle sidebar-collapse" id="sidebar-collapse">
+					<i id="sidebar-toggle-icon" class="ace-icon fa fa-angle-double-left ace-save-state" data-icon1="ace-icon fa fa-angle-double-left" data-icon2="ace-icon fa fa-angle-double-right"></i>
+				</div>
 			</div>
-</div>
