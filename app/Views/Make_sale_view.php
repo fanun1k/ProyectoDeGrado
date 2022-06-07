1a35fbf196aeabf4cc2b1aba9e0081ddf080e8b8
Rodrigo Iriarte <iriartezam18@gmail.com>
2022-06-07 01:47:02 -0400

    Product Sale UI V2

--- app/Views/Make_sale_view.php
+++ app/Views/Make_sale_view.php
@@ -12,21 +12,21 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-6" >
                     <div class="row">
-                        <?php for ($i = 0; $i < 5; $i++) { ?>
+                        <?php foreach ($productsList as $value) { ?>
                         <div class="col-sm-6" style="width:250px;">
-                            <div class="thumbnail search-thumbnail" id="<?php echo $i; ?>" onclick="addToList(this)">
-                                <img class="media-object" src="https://www.196flavors.com/wp-content/uploads/2018/12/pique-macho-3-FP.jpg" />
+                            <div class="thumbnail search-thumbnail" id="card_<?php echo $value->productId; ?>">
+                                <img class="media-object" src="<?php echo base_url("images/product-images")."/".$value->productId.".jpg"; ?>" style="width:200px; height:200px;" />
                                 <div class="row">
                                     <div class="col-sm-6">
-                                        <button class="ace-icon btn btn-success btn-block"><i class="fa fa-plus" aria-hidden="true"></i></button>
+                                        <button class="ace-icon btn btn-success btn-block" id="add_<?php echo $value->productId; ?>" onclick="changeList(this)"><i class="fa fa-plus" aria-hidden="true"></i></button>
                                     </div>
                                     <div class="col-sm-6">
-                                        <button class="ace-icon btn btn-danger btn-block"><i class="fa fa-minus" aria-hidden="true"></i></button>
+                                        <button class="ace-icon btn btn-danger btn-block" id="subtract_<?php echo $value->productId; ?>" onclick="changeList(this)"><i class="fa fa-minus" aria-hidden="true"></i></button>
                                     </div>
                                 </div>
                                 <div class="caption">
-                                    <h3 class="search-title"><a class="blue">Pique</a></h3>
-                                    <p>100 bs</p>
+                                    <h3 class="search-title"><a class="blue"><?php echo $value->productName; ?></a></h3>
+                                    <p><?php echo $value->productPrice; ?> bs.</p>
                                 </div>
                             </div>
                         </div>
@@ -34,38 +34,8 @@
                     </div>
                 </div>
                 <div class="col-xs-12 col-sm-6">
-                    <div class="search-area well well-sm">
-                        <div class="media search-media" style="background-color:white;">
-                            <div class="media-left">
-                                <a><img class="media-object" src="https://www.196flavors.com/wp-content/uploads/2018/12/pique-macho-3-FP.jpg" style="width:80px;"/></a>
-                            </div>
-                            <div class="media-body" style="vertical-align: middle;">
-                                <div>
-                                    <h4 class="media-heading">
-                                        <h3 class="search-title"><a class="blue">Pique</a></h3>
-                                    </h4>
-                                </div>
-
-                                <div class="search-actions text-center">
-                                    <span class="blue bolder bigger-150">3</span>
-                                    <span class="text-info">Bs</span>
-
-                                    <div class="action-buttons bigger-125">
-                                        <a href="#">
-                                            <i class="ace-icon fa fa-phone green"></i>
-                                        </a>
-
-                                        <a href="#">
-                                            <i class="ace-icon fa fa-heart red"></i>
-                                        </a>
-
-                                        <a href="#">
-                                            <i class="ace-icon fa fa-star orange2"></i>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                    <div class="search-area well well-sm" id="productListDiv">
+                        <b class="gray bigger-150">Debe agregar productos a la lista</b>
                     </div>
                 </div>
             </div>
@@ -76,8 +46,7 @@
     <div class="footer-inner">
         <div class="footer-content">
             <span class="bigger-120">
-                <span class="blue bolder">Ace</span>
-                Application &copy; 2013-2014
+                <span class="blue bolder">Ace</span> Application &copy; 2013-2014
             </span>
 
             &nbsp; &nbsp;
@@ -141,16 +110,99 @@
 
 <script type="text/javascript">
 var products = [];
-function addToList(card) {
-    let productId = card.id;
+function changeList(button) {
+    const getIDandAction = (button.id).split("_");
+    let action = getIDandAction[0];
+    let productId = getIDandAction[1];
     let productQuantity = 0;
-    let product = [productId, productQuantity];
-    products.push(product);
+    let productExist = false;
+    let productIndex = -1;
+
+    for (var i = 0; i < Object.keys(products).length; i++) {
+        if(products[i][0] == productId) {
+            productQuantity = products[i][1];
+            productExist = true;
+            productIndex = i;
+        }
+    }
+
+    switch(action) {
+        case "add": productQuantity++;
+            break;
+        case "subtract": productQuantity--;
+            break;
+    }
+
+    if(productQuantity >= 0){
+        product = [productId, productQuantity];
+        if(productExist) products[productIndex] = product;
+        else products.push(product);
+    }
+
+    document.getElementById("productListDiv").innerHTML = "";
+    if(products.length == 0){
+        var addMedia = `<b class="gray bigger-150">Debe agregar productos a la lista</b>`;
+        document.getElementById("productListDiv").appendChild(createElementFromHTML(addMedia));
+    }
+    for (var i = 0; i < products.length; i++) {
+        if(products[i][1] > 0){
+            var addMedia = `<div class="media search-media" style="background-color:white;">
+                    <div class="media-left">
+                        <a><img class="media-object" src="<?php echo base_url("images/product-images")."/"?>` + products[i][0] + `.jpg" style="width:100px; height:100px;"/></a>
+                    </div>
+                    <div class="media-body">
+                        <div>
+                            <h4 class="media-heading">
+                                <h3 class="search-title">
+                                    <div class="row">
+                                        <div class="col-sm-6">
+                                            <a class="blue bolder" style="font-size:18px;">Nombre de Comida Aquí</a>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-sm-6">
+                                            <span class="blue" style="font-size:18px;">Cantidad: </span>
+                                            <span class="blue" style="font-size:18px;">` + products[i][1] + `</span>
+                                        </div>
+                                    </div>
+                                </h3>
+                            </h4>
+                        </div>
+
+                        <div class="search-actions text-center">
+                            
+                            <span class="blue bigger-150">Bs.</span>
+                            <span class="blue bigger-150">?</span>
+                            <div class="action-buttons bigger-125">
+                                <a href="#">
+                                    <i class="ace-icon fa fa-phone green"></i>
+                                </a>
+
+                                <a href="#">
+                                    <i class="ace-icon fa fa-heart red"></i>
+                                </a>
+
+                                <a href="#">
+                                    <i class="ace-icon fa fa-star orange2"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
+            document.getElementById("productListDiv").appendChild(createElementFromHTML(addMedia));
+        }
+    }
+
+    console.log(products);
+}
+
+function createElementFromHTML(htmlString) {
+    var div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+    return div.firstChild;
 }
 
 </script>
-
-<!-- inline scripts related to this page -->
 
 </body>
 
